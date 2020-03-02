8f3ce185bea4e0c9f45630642c3bbb0485197ead
TAKAHASHI Hidetsugu <manzyun@gmail.com>
2020-03-02 10:04:31 +0900

    [Del] Roswell configrate.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -2,10 +2,6 @@
 
 (setq mastodon-instance-url "https://qiitadon.com")
 
-(load (expand-file-name "~/.roswell/helper.el"))
-(setq inferior-lisp-program `((ros ("ros" "run"))                       ; ros run の起動設定
-        (sbcl ("~/.roswell/impls/x86-64/windows/sbcl-bin/1.4.14/bin/sbcl.exe"))))
-
 (set-language-environment "Japanese")
 
 (def-package! ace-window

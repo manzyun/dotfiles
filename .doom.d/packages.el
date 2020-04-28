84868d18de8e2628035eed6a34fad6ca1db2ea1d
TAKAHASHI Hidetsugu <manzyun@gmail.com>
2020-04-28 21:58:05 +0900

    [Add] godot-gdscript with company and yasnippet
    Godoooooooot, I'm comming!! :smiling_imp:

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -6,9 +6,13 @@
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
-(package! evil-little-word :recipe (:fetcher github :repo "tarao/evil-plugins"))
+(package! godot-gdscript.el :recipe (:host github :repo "francogarcia/godot-gdscript.el"))
+(package! company-godot-gdscript :recipe (:host github :repo "francogarcia/company-godot-gdscript.el"))
+(package! yasnippet-godot-gdscript :recipe (:host github :repo "francogarcia/yasnippet-godot-gdscript"))
+
 (package! mastodon)
 (package! org-pomodoro)
 (package! beacon)
+
 (package! eshell-git-prompt)
 (package! minimap)

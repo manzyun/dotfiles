c5383ef673c82ea80a851666d7996751b9b7768e
TAKAHASHI Hidetsugu <manzyun@gmail.com>
2020-04-28 22:00:50 +0900

    [Add][Mod] Godot-GDScript.
    Godooooooooot, I'm comming :smiling_imp:

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,17 +3,40 @@
 (setq mastodon-instance-url "https://qiitadon.com")
 
 (set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+(setq coding-system-for-read 'utf-8)
+(setq coding-system-for-write 'utf-8)
 
-(def-package! ace-window
+(setq doom-theme 'doom-moonlight)
+(setq doom-font(font-spec :family "Ubuntu Mono" :size 12)
+      doom-unicode-font(font-spec :family "Noto Serif CJK JP")
+      doom-big-font (font-spec :size 20))
+
+; TODO: This under place codes for Windows, maybe.
+;(setq doom-theme 'doom-moonlight)
+;(setq doom-font(font-spec :family "JetBrains Mono" :size 12)
+      ;doom-unicode-font(font-spec :family "Meiryo UI")
+      ;doom-big-font (font-spec :size 20))
+
+(use-package! ace-window
   :custom
   (aw-keys '(?a ?o ?e ?u ?h ?t ?n ?s))
   :custom-face
   (aw-leading-char-face((t(:height 4.0 :foreground "#00ff00")))))
 
-(def-package! beacon
+(use-package! beacon
   :custom
   (beacon-color "green")
   :config
   (beacon-mode 1))
 
-(eshell-git-prompt-use-theme 'git-radar)
+;; For Godot Engine.
+(dolist (pattern '("\\.tscn\\'"
+                   ;; Add more extensions here, if needed.
+                   ))
+  (add-to-list 'auto-mode-alist (cons pattern 'toml-mode)))
+
+(add-hook! godot-gdscript-mode company-mode)
+
+(add-hook! godot-gdscript-mode
+          '(lambda () (set (make-local-variable 'yas-indent-line) 'fixed)))

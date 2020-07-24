4b1c6364eda1c046a5fadd8d1d7e8832099e67e3
TAKAHASHI Hidetsugu <manzyun@gmail.com>
2020-07-24 16:37:48 +0900

    [Mod] Shift to gdscript-mode

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -6,9 +6,10 @@
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
-(package! godot-gdscript.el :recipe (:host github :repo "francogarcia/godot-gdscript.el"))
-(package! company-godot-gdscript :recipe (:host github :repo "francogarcia/company-godot-gdscript.el"))
-(package! yasnippet-godot-gdscript :recipe (:host github :repo "francogarcia/yasnippet-godot-gdscript"))
+;; (package! godot-gdscript.el :recipe (:host github :repo "francogarcia/godot-gdscript.el"))
+;; (package! company-godot-gdscript :recipe (:host github :repo "francogarcia/company-godot-gdscript.el"))
+;; (package! yasnippet-godot-gdscript :recipe (:host github :repo "francogarcia/yasnippet-godot-gdscript"))
+(package! gdscript-mode)
 
 (package! exec-path-from-shell)
 (package! mastodon)

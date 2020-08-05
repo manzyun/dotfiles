cdedc36ab93bdaa5f53cbd1d1e1b15c725341e8b
TAKAHASHI Hidetsugu <manzyun@gmail.com>
2020-08-05 13:07:04 +0900

    [Del] Move package configs.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -18,37 +18,7 @@
       ;doom-unicode-font(font-spec :family "Meiryo UI")
       ;doom-big-font (font-spec :size 20))
 
-(use-package! ace-window
-  :custom
-  (aw-keys '(?a ?o ?e ?u ?h ?t ?n ?s))
-  :custom-face
-  (aw-leading-char-face ((t (:height 4.0 :foreground "#00ff00")))))
-
-(use-package! beacon
-  :custom
-  (beacon-color "green")
-  :config
-  (beacon-mode 1))
-
-(use-package! minimap
-  :custom
-  (minimap-window-location 'right)
-  (minimap-update-delay 0.2)
-  (minimap-minimum-width 20))
-
 ;; For private Org-mode documents directory
 (setq my-org-directory "~/Dropbox/org/")
 (setq my-org-agenda-directory "~/Dropbox/org/agenda/")
 (setq org-agenda-files (list my-org-directory my-org-agenda-directory))
-
-;; For Godot Engine.
-;; (dolist (pattern '("\\.tscn\\'"
-;;                    ;; Add more extensions here, if needed.
-;;                    ))
-;;   (add-to-list 'auto-mode-alist (cons pattern 'toml-mode)))
-;;
-;; (add-hook! godot-gdscript-mode company-mode)
-;;
-;; (add-hook! godot-gdscript-mode
-;;           '(lambda () (set (make-local-variable 'yas-indent-line) 'fixed)))
-

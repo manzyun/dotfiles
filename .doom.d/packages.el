fdfb2233b7581a99914ad2fe9340da16b7fbf67c
TAKAHASHI Hidetsugu <manzyun@gmail.com>
2020-08-05 13:07:34 +0900

    [Add] Move package configs.

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -16,3 +16,21 @@
 (package! image-tooltip :recipe
   (:host nil :repo
          "https://gist.github.com/b403e7d0f6d7c459136099759c281b6a.git"))
+
+(use-package! ace-window
+  :custom
+  (aw-keys '(?a ?o ?e ?u ?h ?t ?n ?s))
+  :custom-face
+  (aw-leading-char-face ((t (:height 4.0 :foreground "#00ff00")))))
+
+(use-package! beacon
+  :custom
+  (beacon-color "green")
+  :config
+  (beacon-mode 1))
+
+(use-package! minimap
+  :custom
+  (minimap-window-location 'right)
+  (minimap-update-delay 0.2)
+  (minimap-minimum-width 20))

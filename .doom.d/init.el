a11641fb64cff5d8235f2606b0aa522cd7e6ce4e
TAKAHASHI Hidetsugu <manzyun@gmail.com>
2020-09-11 18:27:27 +0900

    [Add] `+lsp` flag for any language and many flags
    LSP, LSP, LSP...

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -116,7 +116,7 @@
        ;;crystal           ; ruby at the speed of c
        (csharp +lsp)       ; unity, .NET, and mono shenanigans
        data              ; config/data formats
-       (dart +flutter)   ; paint ui and not much else
+       (dart +lsp +flutter)   ; paint ui and not much else
        ;;elixir            ; erlang done right
        elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
@@ -130,7 +130,7 @@
        ;;(haskell +dante) ; a language that's lazier than I am
        hy                ; readability of scheme w/ speed of python
        ;;idris             ;
-       json              ; At least it ain't XML
+       (json +lsp)        ; At least it ain't XML
        (java +meghanada) ; the poster child for carpal tunnel syndrome
        (javascript +lsp)  ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
@@ -139,7 +139,7 @@
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
-       lua               ; one-based indices? one-based indices
+       (lua +lsp)         ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
@@ -148,17 +148,17 @@
        ; organize your plain life in plain text
        (org +brain +journal +pandoc +pomodoro +present +pretty)
 
-       php               ; perl's insecure younger brother
+       (php +lsp)         ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python +lsp +poetry +cython)           ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        raku              ; the artist formerly known as perl6
        rest              ; Emacs as a REST client
        rst               ; ReST in peace
        (ruby +rails)              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)        ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        scheme            ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
@@ -166,7 +166,7 @@
        ;;solidity          ; do you need a blockchain? No.
        swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       web               ; the tubes
+       (web +css +html)   ; the tubes
        yaml              ; JSON, but readable
 
 

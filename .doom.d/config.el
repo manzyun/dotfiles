51d7f407f69bc2baab9e178914ac1e28e550c525
TAKAHASHI Hidetsugu <manzyun@gmail.com>
2020-10-20 11:32:45 +0900

    [Clean] org-agenda-files, any others.
    Maybe my config.el is now like a new razor blade.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,21 +1,17 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
-
-(setq mastodon-instance-url "https://qiitadon.com")
-
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
 (setq coding-system-for-read 'utf-8)
 (setq coding-system-for-write 'utf-8)
 (setq default-input-method "japanese-skk")
+(defvar skk-henkan-show-candidate-keys '(?a ?o ?e ?u ?i ?h ?t ?n ?s))
 (setq system-time-locale "C")
+
 
 (setq doom-theme 'doom-gruvbox)
 (setq doom-font(font-spec :family "Ubuntu Mono" :size 12)
       doom-unicode-font(font-spec :family "Noto Serif CJK JP")
       doom-big-font (font-spec :size 20))
-
-(centaur-tabs-mode t)
-(setq centaur-tabs-style "bar")
 
 ; TODO: This under place codes for Windows, maybe.
 ;(setq doom-theme 'doom-gruvbox)
@@ -23,20 +19,50 @@
       ;doom-unicode-font(font-spec :family "Meiryo UI")
       ;doom-big-font (font-spec :size 20))
 
-;; For private Org-mode documents directory
-;; TODO: If your job site changes, rewrite it for your convenience. Example:
-;; (setq org-agenda-files (f-glob "~/Documents/org/*/*.org"))
-(setq org-agenda-files (f-glob "~/Dropbox/org/*/*.org"))
 
-;(defvar skk-henkan-show-candidate-keys (?a ?o ?e ?u ?i ?h ?t ?n ?s)
-;; FIXME: If not attachment this defvar, Please upper code in Emacs's eval.
+(centaur-tabs-mode t)
+(setq centaur-tabs-style "bar")
+
+
+;; For private Org-mode documents directory
+(setq org-default-notes-file "notes.org")
+(setq org-directory "~/Dropbox/org")
+(setq org-agenda-files (list "~/Dropbox/org"
+                             "~/Dropbox/org/inbox"
+                             "~/Dropbox/org/plans"
+                             "~/Dropbox/org/agenda"
+                             "~/Dropbox/org/outbox"
+                             "~/Dropbox/org/projects"
+                             "~/Dropbox/org/reviews"
+                             "~/Dropbox/org/topics"
+                             "~/Dropbox/org/journal"
+                             "~/Dropbox/org/snippets"
+                             "~/Dropbox/org/archive"))
+
+;; TODO: If your job site changes, rewrite it for your convenience. Example:
+;;(setq org-directory "~/Documents/org")
+;;(setq org-agenda-files (list "~/Documents/org"
+;;                             "~/Documents/org/inbox"
+;;                             "~/Documents/org/plans"
+;;                             "~/Documents/org/agenda"
+;;                             "~/Documents/org/outbox"
+;;                             "~/Documents/org/projects"
+;;                             "~/Documents/org/reviews"
+;;                             "~/Documents/org/topics"
+;;                             "~/Documents/org/journal"
+;;                             "~/Documents/org/snippets"
+;;                             "~/Documents/org/archive"))
+;; Of course. If you want use this config, Running =bootstrap.sh= on your want directory. 
+
+(setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
+
 
 (use-package! ace-window
   :custom
   (aw-keys '(?a ?o ?e ?u ?h ?t ?n ?s))
-  ;; FIXME: If not attachment this :custom, Please ~(setq aw-keys '(?a ?o ?e ?u ?h ?t ?n ?s))~ in Emacs's eval.
   :custom-face
   (aw-leading-char-face ((t (:height 4.0 :foreground "#00ff00")))))
+
 
 (use-package! beacon
   :custom
@@ -44,8 +70,12 @@
   :config
   (beacon-mode 1))
 
+
 (use-package! minimap
   :custom
   (minimap-window-location 'right)
   (minimap-update-delay 0.2)
   (minimap-minimum-width 20))
+
+
+(setq mastodon-instance-url "https://qiitadon.com")
